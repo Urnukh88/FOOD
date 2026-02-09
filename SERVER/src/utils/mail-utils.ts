b4a9cf973035072c2113d3f255358ef840b70000
25LP6949 25LP6949 <urnukhurnuh@gmail.com>
2026-02-09 12:02:18 +0800

    sdsdsdsdasdasD

--- SERVER/src/utils/mail-utils.ts
+++ SERVER/src/utils/mail-utils.ts
@@ -1,28 +1,31 @@
 import nodemailer from "nodemailer";
 import { configDotenv } from "dotenv";
+import { Resend } from "resend";
 configDotenv();
-const { AUTH_EMAIL, AUTH_PASS } = process.env;
-const transport = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: AUTH_EMAIL,
-    pass: AUTH_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendVerificationEmail = async (
   receiver: string,
   verifyEmail: string,
 ) => {
-  await transport.sendMail({
-    from: `"Food Delivery App" <${AUTH_EMAIL}>`,
+  await resend.emails.send({
+    from: "onboarding@resend.dev",
     to: receiver,
     subject: "Verify your email",
     html: `
-<a href="${verifyEmail}">
-  <div style="width:300px;height:300px;background-color:blue;color:white;display:flex;align-items:center;justify-content:center;">
+    <a href="${verifyEmail}">
+    <div style="width:300px;height:300px;background-color:blue;color:white;display:flex;align-items:center;justify-content:center;">
     VERIFY EMAIL
-  </div>
-</a>
-`,
+    </div>
+    </a>
+    `,
   });
 };
+
+// const { AUTH_EMAIL, AUTH_PASS } = process.env;
+// const transport = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: AUTH_EMAIL,
+//     pass: AUTH_PASS,
+//   },
+// });
