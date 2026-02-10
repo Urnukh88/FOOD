8fec575d3d08786836e5651e7e8e6d8c3d25fe74
25LP6949 25LP6949 <urnukhurnuh@gmail.com>
2026-02-10 09:42:17 +0800

    changed

--- SERVER/src/controllers/authentication/reset-password.controller.ts
+++ SERVER/src/controllers/authentication/reset-password.controller.ts
@@ -1,6 +1,41 @@
 import { Request, Response } from "express";
 import { UserModel } from "../../schema";
+import jwt from "jsonwebtoken";
 
-export const authResetPassword = async (req: Request, res: Response) => {};
-try {
-} catch (error) {}
+export const authResetPassword = async (req: Request, res: Response) => {
+  try {
+    const { email } = req.body;
+
+    if (!email) {
+      return res.status(400).json({
+        message: "Email is required",
+      });
+    }
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    const resetToken = jwt.sign(
+      { email: user.email },
+      process.env.JWT_SECRET!,
+      { expiresIn: "10m" },
+    );
+
+    console.log("RESET TOKEN:", resetToken);
+
+    return res.status(200).json({
+      message: "Reset token generated",
+      token: resetToken,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+};
--- SERVER/src/routers/user.router.ts
+++ SERVER/src/routers/user.router.ts
@@ -3,6 +3,7 @@ import {
   createNewUser,
   verifyEmailController,
   resetPassword,
+  authResetPassword,
 } from "../controllers/authentication";
 import { Router } from "express";
 
@@ -10,4 +11,5 @@ export const userRouter = Router();
 userRouter.post("/user-signup", createNewUser);
 userRouter.post("/user-signin", SignIncontroller);
 userRouter.get("/verify-email", verifyEmailController);
-userRouter.post("/pass-request", resetPassword);
+userRouter.post("/request-reset", authResetPassword);
+userRouter.post("/reset-password", resetPassword);
