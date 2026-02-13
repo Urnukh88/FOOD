1db5144fc62be29aa08ef4ec00e3cc86aeef4a3b
25LP6949 25LP6949 <urnukhurnuh@gmail.com>
2026-02-13 09:52:17 +0800

    sadasdasd

--- SERVER/src/controllers/authentication/reset-password-request.controller.ts
+++ SERVER/src/controllers/authentication/reset-password-request.controller.ts
@@ -1,32 +1,3 @@
-// import { Request, Response } from "express";
-// import jwt from "jsonwebtoken";
-// import { UserModel } from "../../schema";
-// import { sendResetTokenEmail } from "../../utils/resetPassword-request-utils";
-
-// export const resetPasswordRequest = async (req: Request, res: Response) => {
-//   try {
-//     const { email } = req.body;
-//     if (!email) return res.status(400).json({ message: "Email required" });
-
-//     const user = await UserModel.findOne({ email });
-//     if (!user) return res.status(404).json({ message: "User oldsongui" });
-
-//     const resetToken = jwt.sign(
-//       { id: user._id },
-//       process.env.JWT_SECRET as string,
-//       { expiresIn: "15m" },
-//     );
-
-//     const resetLink = `https://food-frontend.onrender.com/reset-password?token=${resetToken}`;
-
-//     await sendResetTokenEmail(user.email, resetLink);
-
-//     return res.status(200).json({ message: "Email ilgeegdlee" });
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({ message: "Server error" });
-//   }
-// };
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { UserModel } from "../../schema";
